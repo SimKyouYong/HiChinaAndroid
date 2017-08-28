7b3e37536d128524ef04ec2b778ca20db77fb3fe
심규용 <SKY@simgyuyongui-MacBook-Pro.local>
2017-08-28 22:56:51 +0900

    1.Appara 서버 이전

--- app/src/main/java/sky/kr/co/hichina/common/DEFINE.java
+++ app/src/main/java/sky/kr/co/hichina/common/DEFINE.java
@@ -3,8 +3,8 @@ package sky.kr.co.hichina.common;
 public class DEFINE {
 
 
-    public static final String SERVER_URL       = "http://snap40.cafe24.com/HiChina/php/";
-    public static final String SERVER_IMG_URL   = "http://snap40.cafe24.com/HiChina/img/";
+    public static final String SERVER_URL       = "http://appara.cafe24.com/HiChina/php/";
+    public static final String SERVER_IMG_URL   = "http://appara.cafe24.com/HiChina/img/";
 
 
 
