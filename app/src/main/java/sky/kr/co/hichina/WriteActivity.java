28ceff53c1fc7d4d7f0e1a13c09ee1142fe2fd5c
Sim KyouYong <SKY@Sim-ui-MacBook-Pro.local>
2017-08-30 17:31:56 +0900

    1.글쓰기

--- app/src/main/java/sky/kr/co/hichina/WriteActivity.java
+++ app/src/main/java/sky/kr/co/hichina/WriteActivity.java
@@ -18,22 +18,35 @@ import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import co.kr.sky.AccumThread;
+import sky.kr.co.hichina.common.Check_Preferences;
+import sky.kr.co.hichina.common.DEFINE;
 import sky.kr.co.hichina.obj.ThumbImageInfo;
 
 public class WriteActivity extends FragmentActivity {
     public static Boolean pic_flag = false;
     public static ArrayList<ThumbImageInfo> mThumbImageInfoList_copy = new ArrayList<ThumbImageInfo>();;
+    int serverResponseCode = 0;
 
     private String CATEGORY = "";
     private Button category_btn;
     private EditText title_edit , body_edit;
     private AccumThread mThread;
     private Map<String, String> map = new HashMap<String, String>();
+    private int send_position = 0;
 
     ImageButton imgB[] ;
     FrameLayout fram[];
@@ -144,6 +157,7 @@ public class WriteActivity extends FragmentActivity {
                     break;
                 case R.id.send_btn:
                     Log.e("SKY"  , "--send_btn--");
+                    newSend();
                     break;
                 case R.id.potho_btn:
                     Log.e("SKY"  , "--potho_btn--");
@@ -154,6 +168,45 @@ public class WriteActivity extends FragmentActivity {
             }
         }
     };
+    private void newSend(){
+        send_position = 0;
+        int fullsize = 0;
+        for (int i = 0; i < filename.size(); i++) {
+            if (!filename.get(i).equals("")) {
+                fullsize++;
+            }
+        }
+        Log.e("SKY" , "FF :: " + fullsize);
+        if (fullsize == 0) {
+            //글만 전송
+            //파라미터 전송
+            SimpleDateFormat formatter = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss", Locale.KOREA );
+            Date currentTime = new Date ( );
+            String dTime = formatter.format ( currentTime );
+
+
+            map.put("url", DEFINE.SERVER_URL + "BOARD_WRITE.php");
+            map.put("TITLE",title_edit.getText().toString());
+            map.put("BODY",body_edit.getText().toString());
+            map.put("SELF_ID", Check_Preferences.getAppPreferences(getApplicationContext() ,"MEMBER_ID"));
+            map.put("DATE", dTime);
+            map.put("SELF_ID_KEY_INDEX",Check_Preferences.getAppPreferences(getApplicationContext() ,"KEY_INDEX"));
+            map.put("CATEGORY_1",category_btn.getText().toString());
+
+            for (int i = 0; i < filename.size(); i++) {
+                String[] file_name = filename.get(i).split("/");
+                map.put("image_name" + (i+1),file_name[file_name.length-1]);		//type 에 따른.. 값으로..edit
+            }
+
+            mThread = new AccumThread(WriteActivity.this , mAfterAccum , map , 0 , 0 , null);
+            mThread.start();		//스레드 시작!!
+            return;
+        }else{
+            //사진전송 모두 하고 글 전송
+            AccumThread_i mThread = new AccumThread_i(null,0 , true);
+            mThread.start();
+        }
+    }
     Handler mAfterAccum = new Handler()
     {
         @Override
@@ -167,6 +220,181 @@ public class WriteActivity extends FragmentActivity {
             }
         }
     };
+    //스레드 구현 부분
+    class AccumThread_i extends Thread{
+        Handler mAfter;
+        int i;
+        boolean _a;
+        AccumThread_i(Handler after , int i_ , boolean newsend)
+        {
+            mAfter = after;
+            i = i_;
+            _a = newsend;
+        }
+        @Override
+        public void run()
+        {
+            Log.e("SKY" , "send_position  :: " + send_position);
+
+            if (!filename.get(i).equals("")){
+                Log.i("SKY", "filename.get" + filename.get(i));
+                uploadFile(filename.get(i));
+            }
+
+            send_position++;
+            if (send_position == 10) {
+                if (_a) {
+                    SimpleDateFormat formatter = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss", Locale.KOREA );
+                    Date currentTime = new Date ( );
+                    String dTime = formatter.format ( currentTime );
+
+                    Log.e("SKY" , "filename.size() :: " + filename.size());
+                    Log.e("SKY" , "i :: " + (i+1));
+                    map.put("url", DEFINE.SERVER_URL + "BOARD_WRITE.php");
+                    map.put("TITLE",title_edit.getText().toString());
+                    map.put("BODY",body_edit.getText().toString());
+                    map.put("SELF_ID", Check_Preferences.getAppPreferences(getApplicationContext() ,"MEMBER_ID"));
+                    map.put("DATE", dTime);
+                    map.put("SELF_ID_KEY_INDEX",Check_Preferences.getAppPreferences(getApplicationContext() ,"KEY_INDEX"));
+                    map.put("CATEGORY_1",category_btn.getText().toString());
+
+                    for (int i = 0; i < filename.size(); i++) {
+                        String[] file_name = filename.get(i).split("/");
+                        map.put("IMG_" + (i+1),file_name[file_name.length-1]);		//type 에 따른.. 값으로..edit
+                    }
+
+                    mThread = new AccumThread(WriteActivity.this , mAfterAccum , map , 0 , 0 , null);
+                    mThread.start();		//스레드 시작!!
+
+                }
+            }else{
+                Log.e("SKY" , "전송--------------");
+                AccumThread_i mThread = new AccumThread_i(null , send_position , _a);
+                mThread.start();
+            }
+        }
+    }
+    private int uploadFile(String sourceFileUri) {
+        Log.e("CHECK" , "fileName-->" + sourceFileUri);
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        File sourceFile = new File(sourceFileUri);
+        String[] file_name = sourceFileUri.split("/");
+        //여기서 파일 이름 변경!하고 값 다시 담기!
+        if (!sourceFile.isFile()) {
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    Log.i("SKY", "!sourceFile.isFile() ---> 수정일 때");
+                }
+            });
+            return 0;
+        }
+        else
+        {
+            Log.i("SKY", "sourceFile.isFile() ---> 수정일 때");
+            Log.e("uploadFile", "gogo");
+            Log.e("SKY", "file_name[file_name.length] :: " + file_name[file_name.length-1]);
+            try {
+
+                // open a URL connection to the Servlet
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                URL url = new URL(DEFINE.SERVER_URL + "POTHO_SEND.php");
+
+                // Open a HTTP  connection to  the URL
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoInput(true); // Allow Inputs
+                conn.setDoOutput(true); // Allow Outputs
+                conn.setUseCaches(false); // Don't use a Cached Copy
+                conn.setChunkedStreamingMode(1024);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                conn.setRequestProperty("uploaded_file", sourceFileUri);
+
+                dos = new DataOutputStream(conn.getOutputStream());
+
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";" +
+                        "filename=\""+ file_name[file_name.length-1]+ "" + "\"" + lineEnd);
+
+                dos.writeBytes(lineEnd);
+
+                // create a buffer of  maximum size
+                bytesAvailable = fileInputStream.available();
+
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+                Log.e("uploadFile", "bufferSize-->" + bufferSize);
+
+                // read file and write it into form...
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0) {
+
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                    Log.e("uploadFile", "bufferSize-->" + bufferSize);
+                    Log.e("uploadFile", "bytesRead-->" + bytesRead);
+                    Log.e("uploadFile", "bytesAvailable-->" + bytesAvailable);
+
+
+                }
+
+                // send multipart form data necesssary after file data...
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // Responses from the server (code and message)
+                serverResponseCode = conn.getResponseCode();
+                String serverResponseMessage = conn.getResponseMessage();
+
+                Log.i("uploadFile", "HTTP Response is : "
+                        + serverResponseMessage + ": " + serverResponseCode);
+
+                if(serverResponseCode == 200){
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Log.e("SKY" , "이미지 전송 성공");
+                        }
+                    });
+                }
+                //close the streams //
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+
+            } catch (MalformedURLException ex) {
+                ex.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                    }
+                });
+
+                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                    }
+                });
+                Log.e("SKY", "Exception : "+ e.getMessage());
+            }
+            return serverResponseCode;
+
+        } // End else block
+    }
     @Override
     protected void onDestroy(){
         for (int i = 0; i < imgB.length; i++) {
