c5a93ce77cfc41f663bdcabcd9c6c03a9f4b098e
Sim KyouYong <SKY@Sim-ui-MacBook-Pro.local>
2017-08-31 18:03:52 +0900

    1.리스트불러오기

--- app/src/main/java/sky/kr/co/hichina/SchoolInfoActivity.java
+++ app/src/main/java/sky/kr/co/hichina/SchoolInfoActivity.java
@@ -9,12 +9,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import co.kr.sky.AccumThread;
 import sky.kr.co.hichina.adapter.SchoolInfo_Adapter;
 import sky.kr.co.hichina.common.ActivityEx;
+import sky.kr.co.hichina.common.DEFINE;
+import sky.kr.co.hichina.obj.SchoolInfoObj;
 
 public class SchoolInfoActivity extends ActivityEx {
 
@@ -23,6 +26,8 @@ public class SchoolInfoActivity extends ActivityEx {
     private Map<String, String> map = new HashMap<String, String>();
     private ListView list_number;
     private SchoolInfo_Adapter m_Adapter;
+    private ArrayList<SchoolInfoObj> arr;
+    private String [][] Object_Array;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +49,21 @@ public class SchoolInfoActivity extends ActivityEx {
         findViewById(R.id.tab3_btn).setOnClickListener(btnListener);
         findViewById(R.id.tab4_btn).setOnClickListener(btnListener);
         findViewById(R.id.tab5_btn).setOnClickListener(btnListener);
+
+
+        arr = new ArrayList<SchoolInfoObj>();
+        String val [] = {"Key_Index","Title", "Body", "Date", "Comment_ea",
+                "Comment_id" ,"Comment_nickname", "Image_url" , "Image_urlone", "Image_urltwo",
+                "Image_url4","Image_url5","Image_url6","Image_url7","Image_url8",
+                "Image_url9","Image_url10",
+                "Self_id", "Self_nickname", "choo","City","Tag" ,
+                "Meet_day" , "Meet_time" ,	"Meet_place", 	"Meet_etc" , 	"Meet_etc1" ,
+                "Meet_memo" , 	"Meet_phone" , 	"Meet_point" , "Country" ,"Danger",
+                "Tour_compy","Tour_type","Price","Address","Sale_st",
+                "Sale_end","Menu","Room_type" , "Good" , "My_img"};
+        map.put("url", DEFINE.SERVER_URL + "HellowTalkSel.php");
+        mThread = new AccumThread(this, mAfterAccum , map , 1 , 0 , val);
+
 
 //        m_Adapter = new SchoolInfo_Adapter( SchoolInfoActivity.this , arr , mAfterAccum);
 //        list_number.setAdapter(m_Adapter);
@@ -84,8 +104,38 @@ public class SchoolInfoActivity extends ActivityEx {
         public void handleMessage(Message msg)
         {
             if (msg.arg1  == 0 ) {
-                String res = (String)msg.obj;
-                Log.e("CHECK" , "RESULT  -> " + res);
+                arr.clear();
+                //				Map<String, ArrayList<String>> Object_Array = new HashMap<String, ArrayList<String>>();
+                Object_Array = (String [][]) msg.obj;
+                if (Object_Array.length == 0) {
+                    return;
+                }
+                //				Log.e("CHECK" ,"**********************  --->" + Object_Array[0].length);
+                for (int i = 0; i < Object_Array.length; i++) {
+                    for (int j = 0; j < Object_Array[0].length; j++) {
+                        Log.e("CHECK" ,"value----> ---> Object_Array [" +i+"]["+j+"]"+  Object_Array[i][j]);
+                    }
+                }
+                for (int i = 0; i < (Object_Array[0].length); i++){
+                    if (Object_Array[0][i] != null) {
+//                        arr.add(new HellowTalkobj(Object_Array[0][i],Object_Array[1][i], Object_Array[2][i], Object_Array[3][i],Object_Array[4][i],
+//                                Object_Array[5][i], Object_Array[6][i], Object_Array[7][i],Object_Array[8][i],Object_Array[9][i],
+//                                Object_Array[10][i], Object_Array[11][i],Object_Array[12][i], Object_Array[13][i], Object_Array[14][i],
+//                                Object_Array[15][i],Object_Array[16][i], Object_Array[17][i], Object_Array[18][i],Object_Array[19][i],
+//                                Object_Array[20][i], Object_Array[21][i], Object_Array[22][i], Object_Array[23][i],Object_Array[24][i],
+//                                Object_Array[25][i], Object_Array[26][i], Object_Array[27][i], Object_Array[28][i],	Object_Array[29][i],
+//                                Object_Array[30][i], Object_Array[31][i], Object_Array[32][i], Object_Array[33][i],	Object_Array[34][i],
+//                                Object_Array[35][i], Object_Array[36][i], Object_Array[37][i], Object_Array[38][i],	Object_Array[39][i],
+//                                Object_Array[40][i], Object_Array[41][i]
+//                        ));
+                    }
+                }
+
+//                m_Adapter = new HellowTalkAdapter(arr , mContext , mAfterAccum);
+//                // Xml에서 추가한 ListView 연결
+//                m_ListView.setOnItemClickListener(mItemClickListener);
+//                // ListView에 어댑터 연결
+//                m_ListView.setAdapter(m_Adapter);
 
             }
         }
--- app/src/main/java/sky/kr/co/hichina/obj/SchoolInfoObj.java
+++ app/src/main/java/sky/kr/co/hichina/obj/SchoolInfoObj.java
@@ -7,60 +7,73 @@ public class SchoolInfoObj implements Parcelable{
 	public static Creator<SchoolInfoObj> getCreator() {
 		return CREATOR;
 	}	
-	String key_index;
-	String phone;		
-	String body;		
-	String date;		
-	
-	
-	
-	public SchoolInfoObj(String key_index, String phone, String body, String date) {
-		super();
-		this.key_index = key_index;
-		this.phone = phone;
-		this.body = body;
-		this.date = date;
-	}
-	public String getKey_index() {
-		return key_index;
-	}
-	public void setKey_index(String key_index) {
-		this.key_index = key_index;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	public String getBody() {
-		return body;
-	}
-	public void setBody(String body) {
-		this.body = body;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
+	String KEY_INDEX;
+	String TITLE;
+	String BODY;
+    String SELF_ID;
+    String GOOD_EA;
+    String COMMENT_EA;
+    String DATE;
+    String IMG_1;
+    String IMG_2;
+    String IMG_3;
+    String IMG_4;
+    String IMG_5;
+    String IMG_6;
+    String IMG_7;
+    String IMG_8;
+    String IMG_9;
+    String SELF_ID_KEY_INDEX;
+    String CATEGORY_1;
+
+
 	public SchoolInfoObj(Parcel in) {
 		readFromParcel(in);
 	}
 	@Override
 	public void writeToParcel(Parcel dest, int flags) {
-		dest.writeString(key_index);
-		dest.writeString(phone);
-		dest.writeString(body);
-		dest.writeString(date);
-		
+
+
+		dest.writeString(KEY_INDEX);
+		dest.writeString(TITLE);
+		dest.writeString(BODY);
+        dest.writeString(SELF_ID);
+        dest.writeString(GOOD_EA);
+        dest.writeString(COMMENT_EA);
+        dest.writeString(DATE);
+        dest.writeString(IMG_1);
+        dest.writeString(IMG_2);
+        dest.writeString(IMG_3);
+        dest.writeString(IMG_4);
+        dest.writeString(IMG_5);
+        dest.writeString(IMG_6);
+        dest.writeString(IMG_7);
+        dest.writeString(IMG_8);
+        dest.writeString(IMG_9);
+        dest.writeString(SELF_ID_KEY_INDEX);
+        dest.writeString(CATEGORY_1);
+
 	}
 	private void readFromParcel(Parcel in){
-		key_index = in.readString();
-		phone = in.readString();
-		body = in.readString();
-		date = in.readString();
+
+        KEY_INDEX = in.readString();
+        TITLE = in.readString();
+        BODY = in.readString();
+        SELF_ID = in.readString();
+        GOOD_EA = in.readString();
+        COMMENT_EA = in.readString();
+        DATE = in.readString();
+        IMG_1 = in.readString();
+        IMG_2 = in.readString();
+        IMG_3 = in.readString();
+        IMG_4 = in.readString();
+        IMG_5 = in.readString();
+        IMG_6 = in.readString();
+        IMG_7 = in.readString();
+        IMG_8 = in.readString();
+        IMG_9 = in.readString();
+        SELF_ID_KEY_INDEX = in.readString();
+        CATEGORY_1 = in.readString();
 
 	}
 	@SuppressWarnings("rawtypes")
